587c1a9e19119c2f26f03e6df7ad47b20a3a696b
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-01-29 15:35:47 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,5 +2,8 @@ class Sample{
 
 public static void main(String[] arg){
 System.out.println("Hi Welcome");
+System.out.println("Hi Welcome");
+System.out.println("Hi Welcome");
+System.out.println("Hi Welcome");
 }
 }
